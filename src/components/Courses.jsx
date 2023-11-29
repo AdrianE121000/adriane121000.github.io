41c47eb18bba73685c3727cabf324e7eb3860d98
AdrianE121000 <paracosa3s@gmail.com>
2023-11-29 13:54:08 -0500

    filter by category

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { useContext } from 'react';
-
 import { CoursesContext } from '../context/CoursesContext';
-
 import CoursesCard from './CoursesCard';
 
 const Courses = () => {
--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { useContext } from 'react';
 import { useParams } from 'react-router-dom';
+import { CoursesContext } from '../context/CoursesContext';
+import CoursesCard from './CoursesCard';
 
 const Model = () => {
   const { category } = useParams();
+  const { courses } = useContext(CoursesContext);
+
+  const coursesFilteredByCategory = courses.filter(
+    (course) => course.category === category
+  );
+
+  if (coursesFilteredByCategory.length === 0) {
+    return <div>No hay cursos de esta categoria</div>;
+  }
   return (
     <>
-      <h1>{category}</h1>
+      <CoursesCard courses={coursesFilteredByCategory} />
     </>
   );
 };
