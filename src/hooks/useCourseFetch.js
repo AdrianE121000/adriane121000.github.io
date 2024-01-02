7c71b8dc34fa52e3087e4197e19bdf1f862a0954
AdrianE121000 <paracosa3s@gmail.com>
2024-01-02 10:56:30 -0500

    reset changes

--- src/hooks/useCourseFetch.js
+++ src/hooks/useCourseFetch.js
@@ -1,25 +1,27 @@
 import { useEffect, useState } from 'react';
+import img from '../assets/logo.ico';
 import { useRef } from 'react';
 
 export function useCourseFetch(key) {
   const [course, setCourse] = useState({});
   const [error, setError] = useState();
   const loading = useRef(true);
-  const apiUrl = import.meta.env.VITE_API_URL;
 
   useEffect(() => {
-    fetch(`${apiUrl}/course/${key}`)
-      .then((res) => res.json())
-      .then((json) => {
-        setCourse(json.course);
-      })
-      .catch((error) => {
-        setError(error);
-      })
-      .finally(() => (loading.current = false));
+    setTimeout(() => {
+      fetch(`https://adriane121000.github.io/learnDeals/course.json`)
+        .then((res) => res.json())
+        .then((json) => {
+          setCourse(json.course);
+        })
+        .catch((error) => {
+          setError(error);
+        })
+        .finally(() => (loading.current = false));
+    }, 3000);
   }, [key]);
 
-  const imgCourse = `${apiUrl}/image/${course.image_id}`;
+  const imgCourse = img;
 
   return { course, error, imgCourse, loading };
 }
