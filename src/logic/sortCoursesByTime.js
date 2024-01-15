a946cbce7cafe4e5e009468bfc8273e01602675b
AdrianE121000 <paracosa3s@gmail.com>
2024-01-15 14:57:41 -0500

    fix bug from sort courses by time

--- src/logic/sortCoursesByTime.js
+++ src/logic/sortCoursesByTime.js
@@ -1,34 +1,9 @@
 export function sortByTime(courses) {
-  const sortedCourses = courses.sort((a, b) => {
-    if (
-      Number(
-        a.last_update_date.slice(0, 4) +
-          a.last_update_date.slice(5, 7) +
-          a.last_update_date.slice(8, 10)
-      ) <
-      Number(
-        b.last_update_date.slice(0, 4) +
-          b.last_update_date.slice(5, 7) +
-          b.last_update_date.slice(8, 10)
-      )
-    ) {
-      return 1;
-    }
-    if (
-      Number(
-        a.last_update_date.slice(0, 4) +
-          a.last_update_date.slice(5, 7) +
-          a.last_update_date.slice(8, 10)
-      ) >
-      Number(
-        b.last_update_date.slice(0, 4) +
-          b.last_update_date.slice(5, 7) +
-          b.last_update_date.slice(8, 10)
-      )
-    ) {
-      return -1;
-    }
-    return 0;
+  const sortedCourses = courses.sort(function (a, b) {
+    let dateA = new Date(a.last_update_date);
+    let dateB = new Date(b.last_update_date);
+
+    return dateB - dateA;
   });
 
   return sortedCourses;
